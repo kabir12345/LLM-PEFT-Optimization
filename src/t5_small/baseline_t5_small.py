a89978f4243cf0d11687dd9fc56c9cf497e4c8e4
kabir12345 <kabirjaiswal30@gmail.com>
2023-12-12 23:04:28 -0500

    t5 experiments

--- src/t5_small/baseline_t5_small.py
+++ src/t5_small/baseline_t5_small.py
@@ -1,54 +1,63 @@
 import os
 import torch
-from torch.utils.data import DataLoader
-from transformers import T5ForConditionalGeneration, T5Tokenizer, AdamW
+from torch.utils.data import DataLoader, Subset
+from transformers import T5Tokenizer, T5ForConditionalGeneration, AdamW
 from datasets import load_dataset, DatasetDict
 from torch.nn.utils.rnn import pad_sequence
 from rouge_score import rouge_scorer
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 import nltk
 import logging
+import matplotlib.pyplot as plt
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 nltk.download('punkt')
 
 # Define the path to the cache directory
-cache_dir ="/Users/kabir/Downloads/LLM-PEFT-Optimization/cache"
+cache_dir = "/Users/kabir/Downloads/LLM-PEFT-Optimization/cache/"
 
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
+tokenizer.pad_token = tokenizer.eos_token
 
 def collate_fn(batch):
-    input_ids = pad_sequence([item['input_ids'] for item in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
-    attention_mask = pad_sequence([item['attention_mask'] for item in batch], batch_first=True, padding_value=0)
-    labels = pad_sequence([item['labels'] for item in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
+    input_ids = pad_sequence([torch.tensor(item['input_ids']) for item in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
+    attention_mask = pad_sequence([torch.tensor(item['attention_mask']) for item in batch], batch_first=True, padding_value=0)
+    labels = pad_sequence([torch.tensor(item['labels']) for item in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
     return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
 
-# Load and preprocess the CNN/Daily Mail dataset
-def load_and_preprocess_data(tokenizer):
+def load_and_preprocess_data(tokenizer, train_limit=None, val_limit=None):
     cache_file = os.path.join(cache_dir, "cnn_dailymail_tokenized_t5")
-
     if os.path.exists(cache_file):
         logging.info("Loading tokenized dataset from cache")
-        return DatasetDict.load_from_disk(cache_file)
+        dataset = DatasetDict.load_from_disk(cache_file)
+    else:
+        logging.info("Processing and tokenizing dataset (this may take a while)")
+        dataset = load_dataset("cnn_dailymail", '3.0.0')
+        def tokenize_function(examples):
+            inputs = [tokenizer("summarize: " + article, padding="max_length", truncation=True, max_length=512) for article in examples["article"]]
+            targets = [tokenizer(highlight, padding="max_length", truncation=True, max_length=128) for highlight in examples["highlights"]]
+            
+            # Now, we need to extract the input_ids and attention_mask for each example
+            input_ids = [input['input_ids'] for input in inputs]
+            attention_masks = [input['attention_mask'] for input in inputs]
+            labels = [target['input_ids'] for target in targets]
 
-    logging.info("Processing and tokenizing dataset (this may take a while)")
-    dataset = load_dataset("cnn_dailymail", '3.0.0')
+            return {"input_ids": input_ids, "attention_mask": attention_masks, "labels": labels}
 
-    def tokenize_function(examples):
-        inputs = tokenizer("summarize: " + examples["article"], padding="max_length", truncation=True, max_length=512)
-        targets = tokenizer(examples["highlights"], padding="max_length", truncation=True, max_length=128)
-        return {"input_ids": inputs.input_ids, "attention_mask": inputs.attention_mask, "labels": targets.input_ids}
+        dataset = dataset.map(tokenize_function, batched=True)
+        dataset.save_to_disk(cache_file)
 
-    tokenized_dataset = dataset.map(tokenize_function, batched=True)
-    tokenized_dataset.save_to_disk(cache_file)
-    return tokenized_dataset
+    if train_limit:
+        dataset['train'] = Subset(dataset['train'], range(train_limit))
+    if val_limit:
+        dataset['validation'] = Subset(dataset['validation'], range(val_limit))
 
-# Initialize T5-small model
+    return dataset
+
 def initialize_model():
     return T5ForConditionalGeneration.from_pretrained("t5-small")
 
-# Compute metrics
 def compute_metrics(predictions, references):
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
     rouge_scores = {key: scorer.score(ref, pred)[key].fmeasure for key, pred, ref in zip(['rouge1', 'rouge2', 'rougeL'], predictions, references)}
@@ -56,46 +65,91 @@ def compute_metrics(predictions, references):
     avg_bleu_score = sum(bleu_scores) / len(bleu_scores)
     return rouge_scores, avg_bleu_score
 
+def train_and_evaluate(model, train_loader, val_loader, tokenizer, optimizer, epochs=10):
+    # Lists to keep track of metrics for plotting
+    epoch_losses = []
+    epoch_rouge1_scores = []
+    epoch_rouge2_scores = []
+    epoch_rougeL_scores = []
+    epoch_bleu_scores = []
 
-# Training function
-def train_and_evaluate(model, train_loader, val_dataset, tokenizer, optimizer, epochs=3):
     for epoch in range(epochs):
         model.train()
+        batch_losses = []
         for batch in train_loader:
             inputs, labels = batch['input_ids'], batch['labels']
             attention_mask = batch['attention_mask']
             optimizer.zero_grad()
-            outputs = model(inputs, attention_mask=attention_mask)
-            logits = outputs.logits
-            trimmed_logits = logits[:, :labels.size(1), :]
-            loss = torch.nn.CrossEntropyLoss()(trimmed_logits.view(-1, trimmed_logits.size(-1)), labels.view(-1))
+            outputs = model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
             loss.backward()
             optimizer.step()
+            batch_losses.append(loss.item())
+
+        # Calculate average loss for the epoch
+        avg_loss = sum(batch_losses) / len(batch_losses)
+        epoch_losses.append(avg_loss)
 
         # Evaluation
         model.eval()
         predictions, references = [], []
-        for batch in val_dataset:
-            inputs = batch['input_ids']
-            attention_mask = batch['attention_mask']
-            labels = batch['labels']
+        for batch in val_loader:
+            inputs, attention_mask, labels = batch['input_ids'], batch['attention_mask'], batch['labels']
+
             with torch.no_grad():
-                outputs = model.generate(inputs, attention_mask=attention_mask)
-            decoded_preds = [tokenizer.decode(pred, skip_special_tokens=True) for pred in outputs]
-            decoded_labels = [tokenizer.decode(label, skip_special_tokens=True) for label in labels]
-            predictions.extend(decoded_preds)
+                generated_outputs = model.generate(inputs, attention_mask=attention_mask, max_length=512 + 50)
+                decoded_outputs = [tokenizer.decode(g, skip_special_tokens=True) for g in generated_outputs]
+                decoded_labels = [tokenizer.decode(label, skip_special_tokens=True) for label in labels]
+
+            predictions.extend(decoded_outputs)
             references.extend(decoded_labels)
 
         rouge_scores, avg_bleu_score = compute_metrics(predictions, references)
-        logging.info(f"Epoch {epoch}: ROUGE: {rouge_scores}, BLEU: {avg_bleu_score}")
+        epoch_rouge1_scores.append(rouge_scores['rouge1'])
+        epoch_rouge2_scores.append(rouge_scores['rouge2'])
+        epoch_rougeL_scores.append(rouge_scores['rougeL'])
+        epoch_bleu_scores.append(avg_bleu_score)
 
-# Main function
+        logging.info(f"Epoch {epoch}: Loss: {avg_loss}, ROUGE: {rouge_scores}, BLEU: {avg_bleu_score}")
+
+    # Plotting
+    plt.figure(figsize=(12, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(epoch_losses, label='Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Loss vs Epoch')
+    plt.legend()
+
+    plt.subplot(2, 2, 3)
+    plt.plot(epoch_rouge1_scores, label='ROUGE-1')
+    plt.plot(epoch_rouge2_scores, label='ROUGE-2')
+    plt.plot(epoch_rougeL_scores, label='ROUGE-L')
+    plt.xlabel('Epoch')
+    plt.ylabel('ROUGE Score')
+    plt.title('ROUGE Scores vs Epoch')
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    plt.plot(epoch_bleu_scores, label='BLEU')
+    plt.xlabel('Epoch')
+    plt.ylabel('BLEU Score')
+    plt.title('BLEU Score vs Epoch')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('/Users/kabir/Downloads/LLM-PEFT-Optimization/results/t5-small/baseline_t5.png')  # Update the path to your directory
+    plt.show()
+
 def main():
-    tokenized_dataset = load_and_preprocess_data(tokenizer)
+    TRAIN_DATA_LIMIT = 20  # Adjust this to limit the amount of training data
+    VAL_DATA_LIMIT = 20   # Adjust this to limit the amount of validation data
+    tokenized_dataset = load_and_preprocess_data(tokenizer, train_limit=TRAIN_DATA_LIMIT, val_limit=VAL_DATA_LIMIT)
     model = initialize_model()
     train_loader = DataLoader(tokenized_dataset['train'], batch_size=4, shuffle=True, collate_fn=collate_fn)
+    val_loader = DataLoader(tokenized_dataset['validation'], batch_size=4, shuffle=True, collate_fn=collate_fn)
     optimizer = AdamW(model.parameters(), lr=5e-5)
-    train_and_evaluate(model, train_loader, tokenized_dataset['validation'], tokenizer, optimizer)
+    train_and_evaluate(model, train_loader, val_loader, tokenizer, optimizer)
 
 if __name__ == "__main__":
     main()
--- src/t5_small/optimized_t5_small.py
+++ src/t5_small/optimized_t5_small.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from torch.utils.data import DataLoader, Subset
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, AdamW
+from transformers import T5Tokenizer, T5ForConditionalGeneration, AdamW
 from datasets import load_dataset, DatasetDict
 from torch.nn.utils.rnn import pad_sequence
 from rouge_score import rouge_scorer
@@ -9,20 +9,19 @@ from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 import nltk
 import logging
 import matplotlib.pyplot as plt
-from peft import LoraConfig, IA3Config, get_peft_model 
+from peft import LoraConfig, IA3Config, get_peft_model  # Import PEFT modules
 from torch.optim import AdamW  # Import from PyTorch
-
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 nltk.download('punkt')
 
 # Define the path to the cache directory
-cache_dir = "/Users/kabir/Downloads/LLM-PEFT-Optimization/cache/"  # Update this path
+cache_dir = "/Users/kabir/Downloads/LLM-PEFT-Optimization/cache/"
 
-
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2", padding_side='left')
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
 tokenizer.pad_token = tokenizer.eos_token
+
 def collate_fn(batch):
     input_ids = pad_sequence([torch.tensor(item['input_ids']) for item in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
     attention_mask = pad_sequence([torch.tensor(item['attention_mask']) for item in batch], batch_first=True, padding_value=0)
@@ -30,7 +29,7 @@ def collate_fn(batch):
     return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
 
 def load_and_preprocess_data(tokenizer, train_limit=None, val_limit=None):
-    cache_file = os.path.join(cache_dir, "cnn_dailymail_tokenized")
+    cache_file = os.path.join(cache_dir, "cnn_dailymail_tokenized_t5")
     if os.path.exists(cache_file):
         logging.info("Loading tokenized dataset from cache")
         dataset = DatasetDict.load_from_disk(cache_file)
@@ -38,9 +37,16 @@ def load_and_preprocess_data(tokenizer, train_limit=None, val_limit=None):
         logging.info("Processing and tokenizing dataset (this may take a while)")
         dataset = load_dataset("cnn_dailymail", '3.0.0')
         def tokenize_function(examples):
-            inputs = tokenizer(examples["article"], padding="max_length", truncation=True, max_length=512)
-            targets = tokenizer(examples["highlights"], padding="max_length", truncation=True, max_length=512)
-            return {"input_ids": inputs.input_ids, "attention_mask": inputs.attention_mask, "labels": targets.input_ids}
+            inputs = [tokenizer("summarize: " + article, padding="max_length", truncation=True, max_length=512) for article in examples["article"]]
+            targets = [tokenizer(highlight, padding="max_length", truncation=True, max_length=128) for highlight in examples["highlights"]]
+            
+            # Now, we need to extract the input_ids and attention_mask for each example
+            input_ids = [input['input_ids'] for input in inputs]
+            attention_masks = [input['attention_mask'] for input in inputs]
+            labels = [target['input_ids'] for target in targets]
+
+            return {"input_ids": input_ids, "attention_mask": attention_masks, "labels": labels}
+
         dataset = dataset.map(tokenize_function, batched=True)
         dataset.save_to_disk(cache_file)
 
@@ -51,37 +57,26 @@ def load_and_preprocess_data(tokenizer, train_limit=None, val_limit=None):
 
     return dataset
 
+# Initialize T5 model with LoRA
 def initialize_lora_model():
-    base_model = GPT2LMHeadModel.from_pretrained("gpt2")
+    base_model = T5ForConditionalGeneration.from_pretrained("t5-small")
     lora_config = LoraConfig(
         r=8,  # LoRA rank
         lora_alpha=16,  # LoRA alpha
-        target_modules=["attn.c_attn", "attn.c_proj"],  # Target modules for LoRA
-        fan_in_fan_out=True  # Set to True for GPT-2's Conv1D layers
+        target_modules=["encoder.block.0.layer.0.SelfAttention.q", "encoder.block.0.layer.0.SelfAttention.k"]  # Target modules for LoRA
     )
     lora_model = get_peft_model(base_model, lora_config)
     return lora_model
 
-
-# Initialize GPT-2 model with IA3
+# Initialize T5 model with IA3
 def initialize_ia3_model():
-    base_model = GPT2LMHeadModel.from_pretrained("gpt2")
+    base_model = T5ForConditionalGeneration.from_pretrained("t5-small")
     ia3_config = IA3Config(
-        target_modules=["attn.c_attn", "attn.c_proj", "mlp.c_fc", "mlp.c_proj"],  # Include both attention and feedforward modules
-        feedforward_modules=["mlp.c_fc", "mlp.c_proj"]  # Feedforward modules
+        target_modules=["encoder.block.0.layer.0.SelfAttention.q", "encoder.block.0.layer.0.SelfAttention.k","encoder.block.0.layer.0.DenseReluDense.wi"],  # Target modules for IA3
+        feedforward_modules=["encoder.block.0.layer.0.DenseReluDense.wi"]  # Feedforward modules for IA3
     )
     ia3_model = get_peft_model(base_model, ia3_config)
     return ia3_model
-
-
-def get_optimizer(model):
-    base_params = [p for n, p in model.named_parameters() if not n.endswith("_peft")]
-    peft_params = [p for n, p in model.named_parameters() if n.endswith("_peft")]
-    optimizer = AdamW([
-        {'params': base_params},
-        {'params': peft_params, 'lr': 1e-4}  # Higher learning rate for PEFT parameters
-    ], lr=5e-5)
-    return optimizer
 
 def compute_metrics(predictions, references):
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
@@ -105,10 +100,8 @@ def train_and_evaluate(mod_name,model, train_loader, val_loader, tokenizer, opti
             inputs, labels = batch['input_ids'], batch['labels']
             attention_mask = batch['attention_mask']
             optimizer.zero_grad()
-            outputs = model(inputs, attention_mask=attention_mask)
-            logits = outputs.logits
-            trimmed_logits = logits[:, :labels.size(1), :]
-            loss = torch.nn.CrossEntropyLoss()(trimmed_logits.view(-1, trimmed_logits.size(-1)), labels.view(-1))
+            outputs = model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
             loss.backward()
             optimizer.step()
             batch_losses.append(loss.item())
@@ -139,9 +132,8 @@ def train_and_evaluate(mod_name,model, train_loader, val_loader, tokenizer, opti
 
         logging.info(f"Epoch {epoch}: Loss: {avg_loss}, ROUGE: {rouge_scores}, BLEU: {avg_bleu_score}")
 
-    # Plotting
+    #Plotting
     plt.figure(figsize=(12, 8))
-    plt.title(mod_name)
     plt.subplot(2, 1, 1)
     plt.plot(epoch_losses, label='Loss')
     plt.xlabel('Epoch')
@@ -166,7 +158,7 @@ def train_and_evaluate(mod_name,model, train_loader, val_loader, tokenizer, opti
     plt.legend()
 
     plt.tight_layout()
-    img_path='/Users/kabir/Downloads/LLM-PEFT-Optimization/results/gpt2/optimised_gpt2_'+mod_name+'.png'
+    img_path='/Users/kabir/Downloads/LLM-PEFT-Optimization/results/t5-small/optimized_t5'+mod_name+'.png'
     plt.savefig(img_path)  # Update the path to your directory
     plt.show()
 
@@ -179,17 +171,14 @@ def main():
 
     # Train and evaluate LoRA model
     lora_model = initialize_lora_model()
-    lora_optimizer = get_optimizer(lora_model)
-    lora_model_name="lora"
-    train_and_evaluate(lora_model_name,lora_model, train_loader, val_loader, tokenizer, lora_optimizer)
+    lora_optimizer = AdamW(lora_model.parameters(), lr=5e-5)
+    model_name_lora="lora"
+    train_and_evaluate(model_name_lora,lora_model, train_loader, val_loader, tokenizer, lora_optimizer)
 
     # Train and evaluate IA3 model
     ia3_model = initialize_ia3_model()
-    ia3_optimizer = get_optimizer(ia3_model)
-    ia3_model_name="ia3"
-    train_and_evaluate(ia3_model_name,ia3_model, train_loader, val_loader, tokenizer, ia3_optimizer)
-
-
-
+    ia3_optimizer = AdamW(ia3_model.parameters(), lr=5e-5)
+    model_name_ia3="ia3"
+    train_and_evaluate(model_name_ia3,ia3_model, train_loader, val_loader, tokenizer, ia3_optimizer)
 if __name__ == "__main__":
     main()
